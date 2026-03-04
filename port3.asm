462ec6fcfc93aed6c6d1949ec23cb203d00cf0f8
DianaNikitina <110534493+DianaNikitina@users.noreply.github.com>
2026-03-05 01:10:25 +0300

    Add files via upload

--- port3.asm
+++ port3.asm
@@ -41,137 +41,200 @@ NEXT_LINE               equ 160
 LEFT_COLUMN             equ 720h
 END_CODE                equ 4c00h
 NULL_LEN_STR            equ 00h
-
+SCAN_CODE_SHIFT         equ 036h
+SCAN_CODE_MINUS         equ 04ah
+CLEAN_COLOR             equ 00h
+CLEAN_SYMBOL            equ ' '
 
 Start:      
-                ; ES = 0000 --> zero segment contains int table
-                mov ax, 0
-                mov es, ax
+                        ; ES = 0000 --> zero segment contains int table
+                        mov ax, 0
+                        mov es, ax
 
 
-                ; BX = 4 * 09h --> address of int 09h
-                mov bx, 4 * 09h
+                        ; BX = 4 * 09h --> address of int 09h
+                        mov bx, 4 * 09h
 
 
-                ; save address of int 09h
-                mov ax, es:[bx]
-                mov cs:[OldKeyboardInterruptOffset],  ax
-                mov ax, es:[bx + 2]
-                mov cs:[OldKeyboardInterruptSegment], ax
-                
-                ; es = segment int table
-                mov ax, 0
-                mov es, ax
-                ; address int 09h function in int table (offset)
-                mov bx, 09h*4
-                ; es:[bx] = 0000:0036
-                ;                |
-                ;                |
-                ;               \/
-                ;[][][][][][][][0012:5288][][][][][][]
-                ;[][][][][][][][offset Str_new, cs][][][][][][]
+                        ; save address of int 09h
+                        mov ax, es:[bx]
+                        mov cs:[OldKeyboardInterruptOffset],  ax
+                        mov ax, es:[bx + 2]
+                        mov cs:[OldKeyboardInterruptSegment], ax
+                        
+                        ; es = segment int table
+                        mov ax, 0
+                        mov es, ax
+                        ; address int 09h function in int table (offset)
+                        mov bx, 09h*4
+                        ; es:[bx] = 0000:0036
+                        ;                |
+                        ;                |
+                        ;               \/
+                        ;[][][][][][][][0012:5288][][][][][][]
+                        ;[][][][][][][][offset Str_new, cs][][][][][][]
 
 
-                ; cs:[offset Str_new] = address of our function
+                        ; cs:[offset Str_new] = address of our function
 
 
-                ; clear interrupt flag --> prohibit interruptions
-                cli
-                ; es = 0000
-                ;[][][][][][][][0012:5288][][][][][][]
-                mov word ptr es:[bx], offset Str_new
-                ;[][][][][][][][offset Str_new, 5288][][][][][][]
-                ; <---------------------- can not interrupt
-                ; cs = code segment
-                mov word ptr es:[bx + 2], cs
-                ;[][][][][][][][offset Str_new, cs][][][][][][]
-                ; set interrupt flag --> allow interruptions
-                sti
+                        ; clear interrupt flag --> prohibit interruptions
+                        cli
+                        ; es = 0000
+                        ;[][][][][][][][0012:5288][][][][][][]
+                        mov word ptr es:[bx], offset Str_new
+                        ;[][][][][][][][offset Str_new, 5288][][][][][][]
+                        ; <---------------------- can not interrupt
+                        ; cs = code segment
+                        mov word ptr es:[bx + 2], cs
+                        ;[][][][][][][][offset Str_new, cs][][][][][][]
+                        ; set interrupt flag --> allow interruptions
+                        sti
 
 
-                ; TSR = terminate and stay resident
-                mov ax, 3100h
-                ; DX = memory to stay resident in paragraphs (= 16 bytes)
-                mov dx, offset End_of_programm
-                ; DX / 16
-                shr dx, 4
-                ; + 1 in case not dividable by 16
-                inc dx
-                ; DOS func int
-                int 21h
+                        ; TSR = terminate and stay resident
+                        mov ax, 3100h
+                        ; DX = memory to stay resident in paragraphs (= 16 bytes)
+                        mov dx, offset End_of_programm
+                        ; DX / 16
+                        shr dx, 4
+                        ; + 1 in case not dividable by 16
+                        inc dx
+                        ; DOS func int
+                        int 21h
 
 
-Str_new         proc
+Str_new                 proc
 
-                ;push regs in stack to save value
-                push ax
-                push bx
-                push cx
-                push dx
-                push si
-                push di
-                push ds
-                push es
+                        ;push regs in stack to save value
+                        push ax
+                        push bx
+                        push cx
+                        push dx
+                        push si
+                        push di
+                        push ds
+                        push es
 
-                ;read 60 port in al
-                in al, 60h
-                ;compare al with shift
-                cmp al, 036h
-                jne Check_cleaner
-                call Print_str
-                call Draw_frame
-                jmp Exit_code
+                        ;read 60 port in al
+                        in al, 60h
+                        ;compare al with scancode shift 
+                        cmp al, SCAN_CODE_SHIFT
+                        ;if != check scancode '-'
+                        jne Check_cleaner
+                        ;call func print ax =
+                        call Print_str
+                        ;call func draw frame
+                        call Draw_frame
+                        ;jump label exit_code
+                        jmp Exit_code
 
-Check_cleaner:  cmp al, 04ah
-                call Cleaner
+                        ;compare al with scancode '-'
+Check_cleaner:          cmp al, SCAN_CODE_MINUS
+                        ;if != jump label exit_code
+                        jne Exit_code
+                        ;else call func to clean screen
+                        call Cleaner
 
-
-
-Exit_code       ; AL = value of 61h port (keyboard controller port)
-                in al, 61h
-                ; 80h = 1000 0000b
-                ; set first AL bit to 1 (1st bit == 1 ==> disable keyboard)
-                or al, 80h
-                ; value of 61h port = AL (keyboard controller port)
-                out 61h, al
-                ; AL = 0111 1111b = 7fh
-                ; set first AL bit to 0 (1st bit == 0 ==> enable keyboard)
-                and al, not 80h
-                ; value of 61h port = AL (keyboard controller port)
-                out 61h, al
-
-
-                ; 20h - signal End of Interrupt
-                mov al, 20h ; 0010 0000
-                ; out --> write al in port 20h
-                ; port 20h = Interrupt Controller port
-                out 20h, al
-
-
-                pop  es
-                pop  ds
-                pop  di
-                pop  si
-                pop  dx
-                pop  cx
-                pop  bx
-                pop  ax
-                
-                ; iret ; stack -> offset+segment
+Exit_code:              ; AL = value of 61h port (keyboard controller port)
+                        in al, 61h
+                        ; 80h = 1000 0000b
+                        ; set first AL bit to 1 (1st bit == 1 ==> disable keyboard)
+                        or al, 80h
+                        ; value of 61h port = AL (keyboard controller port)
+                        out 61h, al
+                        ; AL = 0111 1111b = 7fh
+                        ; set first AL bit to 0 (1st bit == 0 ==> enable keyboard)
+                        and al, not 80h
+                        ; value of 61h port = AL (keyboard controller port)
+                        out 61h, al
 
 
-                db 0eah ; jmp far old09ofs:old09seg
-                OldKeyboardInterruptOffset   dw 0
-                OldKeyboardInterruptSegment  dw 0
+                        ; 20h - signal End of Interrupt
+                        mov al, 20h ; 0010 0000
+                        ; out --> write al in port 20h
+                        ; port 20h = Interrupt Controller port
+                        out 20h, al
 
 
-                ; old09ofs dw 0
-                ; old09seg dw 0
-Str_new         endp
+                        pop  es
+                        pop  ds
+                        pop  di
+                        pop  si
+                        pop  dx
+                        pop  cx
+                        pop  bx
+                        pop  ax
+                        
+                        ; iret ; stack -> offset+segment
 
 
-                ret
-                endp
+                        db 0eah ; jmp far old09ofs:old09seg
+                        OldKeyboardInterruptOffset   dw 0
+                        OldKeyboardInterruptSegment  dw 0
+
+
+                        ; old09ofs dw 0
+                        ; old09seg dw 0
+Str_new                 endp
+
+
+                        ret
+                        endp
+;==================================
+Cleaner                 proc
+                        push ax
+                        push bx
+                        push cx
+                        push dx
+
+                        mov ax, VIDEOSEG
+                        mov es, ax
+
+                        ; ' ' on black bg
+                        mov ah, CLEAN_COLOR   
+                        mov al, CLEAN_SYMBOL
+
+                        ;top line of frame 
+                        mov bx, TOP_STR
+
+                        ;quantities str
+                        mov dx, 6              
+
+Clean_Rows:
+                        ;len str frame
+                        mov cl, LEN_AX
+                        ;add len value ax
+                        add cl, cs:[Len_Digit] 
+                        add cl, 6
+                        ;ch = 0              
+                        xor ch, ch
+
+                        ;start current str
+                        push bx             
+
+Clean_Cols:             ;put ' '
+                        mov es:[bx], ax
+                        ;bias (1 byte - symbol, 2 bytes - color)      
+                        add bx, 2              
+                        LOOP Clean_Cols        
+
+                        ;next str
+                        pop bx                 
+                        add bx, NEXT_LINE 
+
+                        ;reduce counter_str
+                        dec dx
+                        ;if != 0 clean next str                 
+                        jnz Clean_Rows       
+
+                        pop dx
+                        pop cx
+                        pop bx
+                        pop ax
+                        ret
+Cleaner                 endp
+
 ;==================================
 ;print ax value
 ;di - save top stack
@@ -201,12 +264,6 @@ conv_num:
 
                         ret
                         endp
-
-;==================================
-
-
-
-
 ;==================================
 ;save bx in global variable - Saved_pos
 ;bx - coord
